a25961af39fb4318b706aa6db3bcd8f20a3978f6
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-19 00:12:35 -0300

    Created medical appointement api, created Doctor and appointment models generated migrations

---
+++ medical_appointment_api/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    med_specialization = models.CharField(
+        max_length=2,
+        choices= [
+            ('OR', 'Orthopedist'),
+            ('OP', 'Ophthalmology'),
+            ('GP', 'General Practitioner')
+        ],
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.med_specialization}"
+
+class Appointment(models.Model):
+    appointment_time = models.TimeField()
+    appointment_date = models.DateField()
+    doctor = models.ForeignKey((Doctor), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.appointment_date}{self.appointment_time}"
+
+

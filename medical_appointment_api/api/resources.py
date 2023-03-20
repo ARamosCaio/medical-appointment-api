1a2e3b6c3189d18c35986c2263779965e48f9d69
Caio Vinícius Araújo Ramos <aramoscaio@gmail.com>
2023-03-19 22:55:35 -0300

    Binding foreign key to doctor property

--- medical_appointment_api/api/resources.py
+++ medical_appointment_api/api/resources.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from api.models import Doctor, Appointment
 from tastypie.authorization import Authorization
+from tastypie import fields
 
 class DoctorResource(ModelResource):
     class Meta:
@@ -10,7 +11,10 @@ class DoctorResource(ModelResource):
 
 
 class AppointmentResource(ModelResource):
+    doctor = fields.ForeignKey(DoctorResource, "doctor")
     class Meta:
         queryset = Appointment.objects.all()
         res_name = 'appointment'        
         authorization = Authorization()
+
+ 
